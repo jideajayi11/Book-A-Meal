7cf389321e734ac7656bd8198a029a25e40eee09
jideajayi11 <jideajayi11@gmail.com>
2018-04-25 11:53:25 +0100

    [#157040328] setup backend

---
+++ server/app.js
@@ -0,0 +1,13 @@
+// import express from "express";
+const express = require('express');
+
+const app=express();
+const port = 3000;
+
+app.get('/',function(req, res){
+res.send("Hello you");
+});
+
+app.listen(port,function(){
+    console.log("port started");
+});
