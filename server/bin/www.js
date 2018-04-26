9ed8ac12cf278d64cb55a66e95842fa51f83f8b3
jideajayi11 <jideajayi11@gmail.com>
2018-04-26 09:03:02 +0100

    add api files

---
+++ server/bin/www.js
@@ -0,0 +1,10 @@
+import * as http from 'http';
+import app from '../app';
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('connected to port');
